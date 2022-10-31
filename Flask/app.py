cc6272a1059a077e388f3a8d06018001b4a978ce
Aki78 <kmorooka78@gmail.com>
2022-10-31 22:03:33 +0200

    created docker

--- Flask/app.py
+++ Flask/app.py
@@ -13,11 +13,9 @@ stores = [
             }
         ]
 
-
 @app.get("/store") #http://127.0.0.1:500/store
 def get_stores():
     return {"stores": stores}
-
 
 @app.post("/store") #http://127.0.0.1:500/store
 def create_store():
@@ -26,9 +24,27 @@ def create_store():
     stores.append(new_store)
     return new_store, 201
 
-
 @app.post("/store/<string:name>/item")
 def create_item(name):
-    pass
+    request_data = request.get_json()
+
+    for store in stores:
+        if store["name"] == name:
+            new_item = {"name": request_date["name"], "price": request_data["price"]}
+            store["items"].append(new_item)
+            return nre_item, 201
+    return {"message": "Store not found"}, 404
+
+@app.get("/store/<string:name>/item")
+def get_item(name):
+    request_data = request.get_json()
+
+    for store in stores:
+        if store["name"] == name:
+            new_item = {"name": request_date["name"], "price": request_data["price"]}
+            store["items"].append(new_item)
+            return {"items": store["items"], "message": "you rock!"}
+    return {"message": "Store not found"}, 404
+
 
 app.run()
